30cae7c45aa6fcde32c0af62086bf64fe22e9f64
Enochio <enochkobbysnr@gmail.com>
2023-06-22 14:58:37 -0700

    Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * mul _ Multiplies two integers
+ * mul - Multiplies two integers
  * @x: The first number to multiply
  * @y: The second number to multiply with the first number
  * Return: 1 for a character that will be a digit or 0 for any else
